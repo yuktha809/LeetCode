2f764edb07c0533cc0eaeac8f03b86f29d6a733d
yuktha809 <yuktha809@gmail.com>
2026-07-06 18:49:35 +0530

    LC-reverse-integer [Runtime: N/A; Memory: N/A]

--- math/reverse-integer.cpp
+++ math/reverse-integer.cpp
@@ -6,11 +6,11 @@ public:
         while(x>0){
             int rem = x % 10;
             x = x/10;
-           if (rev > INT_MAX / 10 || (rev == INT_MAX / 10 && digit > 7))
+           if (revN > INT_MAX / 10 || (revN == INT_MAX / 10 && digit > 7))
                 return 0;
 
             
-            if (rev < INT_MIN / 10 || (rev == INT_MIN / 10 && digit < -8))
+            if (revN < INT_MIN / 10 || (revN == INT_MIN / 10 && digit < -8))
                 return 0;
             revN = (revN*10)+rem;
             
