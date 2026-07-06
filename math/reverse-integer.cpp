30b376cd95eac3da80bfe641047873ac876035aa
yuktha809 <yuktha809@gmail.com>
2026-07-06 18:45:01 +0530

    LC-reverse-integer [Runtime: N/A; Memory: N/A]

--- math/reverse-integer.cpp
+++ math/reverse-integer.cpp
@@ -4,7 +4,7 @@ public:
         int revN=0;
         while(x>0){
             int rem = x % 10;
-            rev = (rev*10)+rem;
+            revN = (rev*10)+rem;
             n=n/10;
         }
         return 0;
