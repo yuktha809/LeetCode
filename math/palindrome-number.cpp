4c0b9aebe842c09799df33cf9504fbb123d00b60
yuktha809 <yuktha809@gmail.com>
2026-07-06 19:02:28 +0530

    LC-palindrome-number [Runtime: N/A; Memory: N/A]

--- math/palindrome-number.cpp
+++ math/palindrome-number.cpp
@@ -4,10 +4,13 @@ public:
         if(x<0){
             return false;
         }
-        int rev=0;
+        int rev = 0;
         int num = x;
         while(x > 0){
-            int rem = x%10;
+            int rem = x % 10;
+            if(rev > INT_MAX/10 || (rev = INT_MAX/10 && rem > 7)){
+                return false;
+            }
             rev = (rev*10)+rem;
             x=x/10;
         }
