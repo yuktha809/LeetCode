8f8707d6b8596ee1674063080ad1ab251f494149
yuktha809 <yuktha809@gmail.com>
2026-07-07 13:59:02 +0530

    LC-majority-element [Runtime: N/A; Memory: N/A]

--- array/majority-element.cpp
+++ array/majority-element.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
        unordered_map<int,int> mp;
-       for(n:nums){
+       for(int n:nums){
         mp[n]++;
         if(mp[n]> (nums.size()/2){
             return n;
