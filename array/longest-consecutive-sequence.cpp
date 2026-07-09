42fce721201219ccb10679531b53ec2a1ea2afc2
yuktha809 <yuktha809@gmail.com>
2026-07-09 13:55:49 +0530

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

---
+++ array/longest-consecutive-sequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> sr(nums.begin(),nums,end());
+        int longest = 1;
+     for(int num :nums){
+        if(st.find(num-1) == st.end()){
+            int current = num;
+            int lenght=1;
+            while(st.find(current+1)!=st.end()){
+                current ++;
+                lenght++;
+            }
+            longest=max(longest,lenght);
+        }
+     }
+        return longest;
+    }
+};
