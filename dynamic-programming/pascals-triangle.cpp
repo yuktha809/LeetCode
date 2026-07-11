e2026ac206357754c48e05be3c4e857b36e5818d
yuktha809 <yuktha809@gmail.com>
2026-07-11 13:49:53 +0530

    LC-pascals-triangle [Runtime: N/A; Memory: N/A]

--- dynamic-programming/pascals-triangle.cpp
+++ dynamic-programming/pascals-triangle.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        vector<int> ans;
+        vector<vector<int>> ans;
         for(int i =0;i<numRows;i++){
             vector<int> row(i+1,1);
             for(int j = 1;j<i;j++){
