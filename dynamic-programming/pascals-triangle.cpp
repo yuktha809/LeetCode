850c2f3328e128611c74c9c50a02f0f4a46f0416
yuktha809 <yuktha809@gmail.com>
2026-07-11 13:50:31 +0530

    LC-pascals-triangle [Runtime: N/A; Memory: N/A]

--- dynamic-programming/pascals-triangle.cpp
+++ dynamic-programming/pascals-triangle.cpp
@@ -7,6 +7,7 @@ public:
             for(int j = 1;j<i;j++){
                 row[j]=ans[i-1][j-1] + ans[i-1][j];
             }
+            ans.push_back(row);
         }
         return ans;
     }
