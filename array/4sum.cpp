835ccc44b625973b62c6466897f469a2b4768e8d
yuktha809 <yuktha809@gmail.com>
2026-07-12 20:41:42 +0530

    LC-4sum [Runtime: N/A; Memory: N/A]

--- array/4sum.cpp
+++ array/4sum.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
+        sort(nums.begin(),nums.end());
         vector<vector<int>> ans;
         for(int i =0;i<n-3;i++){
             if(i>0 && nums[i]==nums[i-1])
@@ -12,15 +13,16 @@ public:
                 int left = j+1;
                 int right = nums.size()-1;
                 while(left<right){
-                    int sum = nums[i]+nums[j]+nums[left]+nums[right];
+                    long long sum = (long long)nums[i]+nums[j]+nums[left]+nums[right];
                     if(sum==target){
                         ans.push_back({nums[i],nums[j],nums[left],nums[right]});
+                        left++;right--;
                         while(left<right && nums[left]==nums[left-1])
                         left++;
                         while(left< right && nums[right]==nums[right+1])
                         right--;
                     }
-                    else if(sum<0){
+                    else if(sum<target){
                         left++;
                     }
                     else{
