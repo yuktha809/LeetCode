6c6acb969a90215fca3cb14b0420dd4469ed282f
yuktha809 <yuktha809@gmail.com>
2026-07-12 12:55:36 +0530

    LC-merge-intervals [Runtime: N/A; Memory: N/A]

--- array/merge-intervals.cpp
+++ array/merge-intervals.cpp
@@ -5,7 +5,7 @@ public:
          vector<vector<int>> ans;
          for(int i =0;i<n;i++){
             if(ans.empty() || ans[i][0]>ans.back()[1]){
-                ans.pushback(intervals[i]);
+                ans.push_back(intervals[i]);
             }
             else{
                 ans.back()[1]= max(ans.back()[1],arr[i][1]);
