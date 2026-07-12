73bb73dcff74ca9de5c8bcf42e17a2823a9b7abb
yuktha809 <yuktha809@gmail.com>
2026-07-12 20:37:39 +0530

    LC-4sum [Runtime: N/A; Memory: N/A]

---
+++ sorting/4sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
+        for(int i =0;i<n-3;i++){
+            if(i>0 && nums[i]==nums[i-1])
+            continue;
+            for(int j = i+1;j<n-2;j++){
+                if(j>i+1 && nums[j]==nums[j-1])
+                continuel
+                int left = j+1;
+                int right = nums.size()-1;
+                while(left<right){
+                    int sum = nums[i]+nums[j]+nums[left]+nums[right];
+                    if(sum==target){
+                        ans.push_back(nums[i],nums[j],nums[left],nums[right]);
+                        while(left<right && nums[left]==nums[left-1])
+                        left++
+                        while(leftn< right && nums[right]==nums[right+1])
+                        right--;
+                    }
+                    else if(sum<0){
+                        left++;
+                    }
+                    else{
+                        right--;
+                    }
+                }
+            }
+       
+     }
+    return ans;
+    }
+};
