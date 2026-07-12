cbeebe1ebc716aab7f61833a11e15ce44f2864f0
yuktha809 <yuktha809@gmail.com>
2026-07-12 20:38:55 +0530

    LC-4sum [Runtime: N/A; Memory: N/A]

--- two-pointers/4sum.cpp
+++ two-pointers/4sum.cpp
@@ -14,7 +14,7 @@ public:
                 while(left<right){
                     int sum = nums[i]+nums[j]+nums[left]+nums[right];
                     if(sum==target){
-                        ans.push_back(nums[i],nums[j],nums[left],nums[right]);
+                        ans.push_back({nums[i],nums[j],nums[left],nums[right]});
                         while(left<right && nums[left]==nums[left-1])
                         left++
                         while(leftn< right && nums[right]==nums[right+1])
