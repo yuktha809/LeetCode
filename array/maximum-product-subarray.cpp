26071ec89b9e8f5346c56e42905b6a8c7108dae4
yuktha809 <yuktha809@gmail.com>
2026-07-13 21:53:03 +0530

    LC-maximum-product-subarray [Runtime: N/A; Memory: N/A]

---
+++ array/maximum-product-subarray.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int pre=1,suff=1;
+        int ans=INT_MIN;
+        for(int i = 0;i<nums.size()-1;i++){
+            if(pre==0) {
+                pre = 1;
+            }
+            if(suff==0){
+                suff=1;
+            }
+            pre *=nums[i];
+            suff *=nums[n-i-1];
+
+             n=max(pre,suff);
+             ans = max(ans,n);
+        }
+       return ans;
+    }
+};
