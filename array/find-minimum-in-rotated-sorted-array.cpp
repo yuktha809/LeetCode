b41a36b0dab98e5ade4d41bb245019cc18e5bfca
yuktha809 <yuktha809@gmail.com>
2026-07-16 14:45:55 +0530

    LC-find-minimum-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

---
+++ array/find-minimum-in-rotated-sorted-array.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int low = 0,high = nums.size()-1;
+        int ans  = -1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[low]<=nums[mid]){
+                ans = min(ans,nums[low]);
+            }
+            else{
+                ans= min(ans,nums[mid]);
+                high = mid-1;
+            }
+        }
+        return ans;
+        
+    }
+};
