7673b5118e48f70a40b02e790a5dcbcf53c01831
yuktha809 <yuktha809@gmail.com>
2026-07-16 14:47:07 +0530

    LC-find-minimum-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

--- binary-search/find-minimum-in-rotated-sorted-array.cpp
+++ binary-search/find-minimum-in-rotated-sorted-array.cpp
@@ -7,10 +7,12 @@ public:
             int mid = (low+high)/2;
             if(nums[low]<=nums[mid]){
                 ans = min(ans,nums[low]);
+                low = mid+1;
             }
             else{
-                ans= min(ans,nums[mid]);
                 high = mid-1;
+                ans= min(ans,nums[mid]);
+                
             }
         }
         return ans;
