de253663300c09d90d33bfd492ecbe1be64c0260
yuktha809 <yuktha809@gmail.com>
2026-07-17 20:36:28 +0530

    LC-koko-eating-bananas [Runtime: N/A; Memory: N/A]

---
+++ array/koko-eating-bananas.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countTotalHour(vector<int>& piles,int speed){
+        int totalH=0;
+        for(int i: piles){
+            totalH += ceil(i/speed);
+        }
+        return totalH;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int maxpile = *max_element(pile.begin(),piles.end());
+        int low =0,high = maxpile;
+        int ans = INT_MAX;
+       
+        while(low<high){
+            int mid = (low+high)/2;
+             int time=countTotalHour(piles,mid);
+            if(time>=h){
+                ans = mid;
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+};
