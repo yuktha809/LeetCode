335a32fc85b97a6994c8a5d18fc9919f708e5783
yuktha809 <yuktha809@gmail.com>
2026-07-17 14:17:29 +0530

    LC-find-peak-element [Runtime: N/A; Memory: N/A]

--- array/find-peak-element.cpp
+++ array/find-peak-element.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int low =0;
-        high = nums.size()-1;
+        int high = nums.size()-1;
         while(low<=high){
             int mid = (low+high)/2;
             if(nums[mid]<nums[mid+1]){
