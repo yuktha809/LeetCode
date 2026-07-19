5a139e7ae172fec95702b927a0a18fd9316fc9c4
yuktha809 <yuktha809@gmail.com>
2026-07-19 14:50:23 +0530

    LC-smallest-subsequence-of-distinct-characters [Runtime: N/A; Memory: N/A]

--- string/smallest-subsequence-of-distinct-characters.cpp
+++ string/smallest-subsequence-of-distinct-characters.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
     string smallestSubsequence(string s) {
-        for(int i :s){
-            for(int j=i;j<len(s);j++){
-                 if(s[i]!=s[j])
-                 return s[i];
+        vector<int> last(26);
+
+        for (int i = 0; i < s.size(); i++)
+            last[s[i] - 'a'] = i;
+
+        vector<bool> used(26, false);
+        string ans;
+
+        for (int i = 0; i < s.size(); i++) {
+            char c = s[i];
+
+            if (used[c - 'a'])
+                continue;
+
+            while (!ans.empty() &&
+                   ans.back() > c &&
+                   last[ans.back() - 'a'] > i) {
+                used[ans.back() - 'a'] = false;
+                ans.pop_back();
             }
-           
+
+            ans.push_back(c);
+            used[c - 'a'] = true;
         }
-        
+
+        return ans;
     }
 };
