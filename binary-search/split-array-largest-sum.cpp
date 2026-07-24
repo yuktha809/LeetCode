4dc45cf9d90154231868deb2ae28b84f4bc50902
yuktha809 <yuktha809@gmail.com>
2026-07-24 12:54:07 +0530

    LC-split-array-largest-sum [Runtime: N/A; Memory: N/A]

---
+++ binary-search/split-array-largest-sum.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int numPartitions(vector<int>& nums,int maxSum ){
+        int partition=1;
+        int sum = 0;
+        for(int i =0;i<nums.size();i++){
+            if(nums[i] + sum <= maxSum){
+                nums[i] +=sum;
+            }
+            else{
+                partition++;
+                sum = nums[i];
+
+            }
+        }
+    return partition;
+    }
+    int splitArray(vector<int>& nums, int k) {
+       
+        int low = *max_element(nums.begin(),nums.end());
+        int high = accumulate(nums.begin(),nums.end());
+        for(int i =low;i<=high;i++){
+            if(numPartitions(nums,i)==k){
+                return i;
+            }
+        }
+        return low;
+    }
+};
