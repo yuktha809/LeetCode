fea7c38dd18a9801e5d8b0f49c4cd7207bc5019d
yuktha809 <yuktha809@gmail.com>
2026-07-27 13:34:33 +0530

    LC-median-of-two-sorted-arrays [Runtime: N/A; Memory: N/A]

--- divide-and-conquer/median-of-two-sorted-arrays.cpp
+++ divide-and-conquer/median-of-two-sorted-arrays.cpp
@@ -15,7 +15,7 @@ public:
             int r1=INT_MAX,r2=INT_MAX;
             if(mid1<m) r1=nums1[mid1];
             if(mid2<n) r2 = nums2[mid2];
-            if(mid-1>=0) l1 = nums1[mid1-1];
+            if(mid1-1>=0) l1 = nums1[mid1-1];
             if(mid2-1>=0) l2 = nums2[mid2-1];
             if(l1<r2 && l2<r1){
                 if((m+n)/2 == 1){
