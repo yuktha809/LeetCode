ad9c4fe3f2dca832276519d914bc1854b41ac616
yuktha809 <yuktha809@gmail.com>
2026-08-03 18:49:06 +0530

    LC-largest-odd-number-in-string [Runtime: N/A; Memory: N/A]

--- string/largest-odd-number-in-string.cpp
+++ string/largest-odd-number-in-string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string largestOddNumber(string num) {
         for(int i = num.size()-1;i>=0;i--){
-            if(num[i]-'0'%2 ==1){
+            if((num[i]-'0')%2 ==1){
                 num.substr(0,i+1);
             }
         }
