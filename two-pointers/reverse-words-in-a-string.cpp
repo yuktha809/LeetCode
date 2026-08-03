3666e4ccd2e3b40cf6f39fe0985158238ab66471
yuktha809 <yuktha809@gmail.com>
2026-08-03 18:34:52 +0530

    LC-reverse-words-in-a-string [Runtime: N/A; Memory: N/A]

--- two-pointers/reverse-words-in-a-string.cpp
+++ two-pointers/reverse-words-in-a-string.cpp
@@ -5,7 +5,7 @@ public:
         int start = 0;
         for(int end =0;end<s.size();end++){
             if(end!=s.size() || s[end]==' '){
-                reverse(s.begin()+start,s.end()+end);
+                reverse(s.begin()+start,s.begin()+end);
                 start = end +1;
             }
         }
