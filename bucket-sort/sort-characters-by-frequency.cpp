2fba72f4d0d04642826dcaa9b89cb8802b521ec3
yuktha809 <yuktha809@gmail.com>
2026-08-06 21:48:58 +0530

    LC-sort-characters-by-frequency [Runtime: N/A; Memory: N/A]

---
+++ bucket-sort/sort-characters-by-frequency.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        unordered_map<int,char> freq;
+        for(char ch:s){
+            freq[ch]++;
+        }
+        priority_queue<pair<char,int>>pq;
+        for(auto i:freq){
+            pq.push({i.second,i.first});
+        }
+        string ans="";
+        while(!pq.empty()){
+            int count = pq.top.first();
+            char ch = pq.top.second();
+            pq.pop();
+            ans = append(count,ch);
+        }
+        return ans;
+
+    }
+};
