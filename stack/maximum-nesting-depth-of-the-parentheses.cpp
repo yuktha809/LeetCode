637bfdfb47224006e9d7ec5e195629b736a62bee
yuktha809 <yuktha809@gmail.com>
2026-08-06 22:07:20 +0530

    LC-maximum-nesting-depth-of-the-parentheses [Runtime: N/A; Memory: N/A]

--- stack/maximum-nesting-depth-of-the-parentheses.cpp
+++ stack/maximum-nesting-depth-of-the-parentheses.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxDepth(string s) {
         int depth=0,maxDepth=0;
-        for(char c:s){
+        for(char ch:s){
             if(ch == '('){
                 depth++;
                 maxDepth=max(depth,maxDepth);
