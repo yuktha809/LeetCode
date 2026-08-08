4afee6e8cfdc5e51f392bca0ee9d54222cdc262a
yuktha809 <yuktha809@gmail.com>
2026-08-08 15:05:37 +0530

    LC-string-to-integer-atoi [Runtime: N/A; Memory: N/A]

---
+++ string/string-to-integer-atoi.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i =0;
+        int n = s.size();
+        while(i<n && s[i]==' '){
+            i++;
+
+            int sign = 1;
+            if(i<n && (s[i]=='+' || s[i]=='-'){
+                if(s[i]=='-'){
+                    sign =-1;
+                
+              }
+            }
+        
+        long long ans = 0;
+        while(i<n && isdigit(s[i])){
+            ans = ans*10+(s[i]-'0');
+            if(ans*sign > INT_MAX) return INT_MAX;
+            else return INT_MIN;
+        i++;
+        }
+        }
+        return ans;
+    }
+};
