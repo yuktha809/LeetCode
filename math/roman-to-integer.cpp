26b9dde0c0e50ff7bc6bb46d960f1abf73633d9b
yuktha809 <yuktha809@gmail.com>
2026-08-08 13:23:36 +0530

    LC-roman-to-integer [Runtime: N/A; Memory: N/A]

--- math/roman-to-integer.cpp
+++ math/roman-to-integer.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int romanToInt(string s) {
         unordered_map<char,int> roman ={
-            {I,1},{V,5},{X,10},{L,50},{C,100},{D,500}{M,1000}
+            {'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}
         };
         int ans = 0;
         for(int i =0;i<s.size();i++){
