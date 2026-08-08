e36eee3ced0986f5b9907bfcd86d117b6e91f70b
yuktha809 <yuktha809@gmail.com>
2026-08-08 13:22:45 +0530

    LC-roman-to-integer [Runtime: N/A; Memory: N/A]

--- hash-table/roman-to-integer.cpp
+++ hash-table/roman-to-integer.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char,int> romab ={
+        unordered_map<char,int> roman ={
             {I,1},{V,5},{X,10},{L,50},{C,100},{D,500}{M,1000}
         };
         int ans = 0;
