1b3a7ac123fa1e15301aa5f974760c0aa0584a6c
yuktha809 <yuktha809@gmail.com>
2026-08-11 21:27:28 +0530

    LC-reverse-linked-list [Runtime: N/A; Memory: N/A]

--- linked-list/reverse-linked-list.cpp
+++ linked-list/reverse-linked-list.cpp
@@ -12,15 +12,15 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         
-        ListNode* prev=NULL;
-        ListNode* temp=head;
-        while(temp!=NULL){
-            ListNode* front = temp->next;
-            temp->next=prev;
-            prev=temp;
-            temp=front;
-        }
-    return prev;
+       ListNode* prev=NULL;
+       ListNode* temp =head;
+       while(temp!=NULL){
+        ListNode* front = temp->next;
+        temp->next=prev;
+        prev=temp;
+        temp=front;
+       }
+       return prev;
     }
 
 };
