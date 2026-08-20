36d1b31b502302c880ac50d3f3c94d3501a85a06
yuktha809 <yuktha809@gmail.com>
2026-08-20 19:41:18 +0530

    LC-add-two-numbers [Runtime: N/A; Memory: N/A]

--- math/add-two-numbers.cpp
+++ math/add-two-numbers.cpp
@@ -12,7 +12,7 @@ class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode* dummy= new ListNode(0);
-        ListNode* temp=dummy1;
+        ListNode* temp=dummy;
         int carry=0;
         while(l1!=NULL || l2!=NULL || carry!=0){
             int sum = carry;
