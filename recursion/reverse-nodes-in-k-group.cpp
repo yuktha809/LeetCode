22b3d8878efcf64846967fcb52abd558cce2e37a
yuktha809 <yuktha809@gmail.com>
2026-08-25 17:49:39 +0530

    LC-reverse-nodes-in-k-group [Runtime: N/A; Memory: N/A]

---
+++ recursion/reverse-nodes-in-k-group.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        ListNode* curr=head;
+        ListNode* prev =NULL;
+        ListNode* next=NULL;
+        int count=0;
+        while(curr!=0 && count<k){
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+            count++;
+        } 
+    }
+    return prev;
+};
