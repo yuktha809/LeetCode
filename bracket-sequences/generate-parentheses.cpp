798d59570f654cf680857972c1b67f59b54f490a
yuktha809 <yuktha809@gmail.com>
2026-08-27 22:11:45 +0530

    LC-generate-parentheses [Runtime: N/A; Memory: N/A]

---
+++ bracket-sequences/generate-parentheses.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector solve(int open,int close,int n,,string s,vector<string>& ans){
+        if(s.lenght()==2*n){
+            ans.push_back(s);
+            return;
+        }
+        if(open<n){
+            solve(open+1,close,n,s+'(',ans);
+        }
+        if(close<open){
+            solve(open,close+1,s+')',ans)
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string>& ans;
+        solve(0,0,n,'',ans);
+        return ans;
+    }
+};
