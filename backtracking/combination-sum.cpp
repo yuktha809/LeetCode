6c6d8e2caafd3af112d5c165a4d02b9410aa9618
yuktha809 <yuktha809@gmail.com>
2026-08-30 15:44:39 +0530

    LC-combination-sum [Runtime: N/A; Memory: N/A]

--- backtracking/combination-sum.cpp
+++ backtracking/combination-sum.cpp
@@ -10,6 +10,7 @@ public:
             return;
         }
         if(arr[index]<=target){
+            da.push_back(arr[index]);
             combos(index,target-arr[index],arr,ds,ans);
             ds.pop_back();
         }
