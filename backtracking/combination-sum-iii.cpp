c57e7b23fd40d45adab60964bc333dd8ab729dc5
yuktha809 <yuktha809@gmail.com>
2026-09-01 18:05:14 +0530

    LC-combination-sum-iii [Runtime: N/A; Memory: N/A]

--- backtracking/combination-sum-iii.cpp
+++ backtracking/combination-sum-iii.cpp
@@ -1,5 +1,5 @@
 class Solution {
-private:
+public:
     void combos(int sum,int last,int k,vector<int>& nums,vector<vector<int>>& ans){
         if(sum==0 && nums.size()==k){
             ans.push_back(nums);
@@ -19,7 +19,6 @@ private:
             }
         }
     }
-public:
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> ans;
         vector<int> nums;
