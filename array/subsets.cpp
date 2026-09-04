74a2c8309b915102501529d580b6a007ce652c8d
yuktha809 <yuktha809@gmail.com>
2026-09-04 12:13:51 +0530

    LC-subsets [Runtime: N/A; Memory: N/A]

--- array/subsets.cpp
+++ array/subsets.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
+        int n = nums.size();
         int total = 1 << n;
          vector<vector<int>> ans;
         for(int mask=0;mask<total;mask++){
