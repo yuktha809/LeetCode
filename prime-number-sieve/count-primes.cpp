ef8f9feeb98857d6ba5ba270df65d28b61a207d1
yuktha809 <yuktha809@gmail.com>
2026-09-06 15:06:02 +0530

    LC-count-primes [Runtime: N/A; Memory: N/A]

--- prime-number-sieve/count-primes.cpp
+++ prime-number-sieve/count-primes.cpp
@@ -5,22 +5,32 @@ public:
         if (n <= 2)
             return 0;
 
-        vector<bool> isPrime(n, true);
-        isPrime[0] = isPrime[1] = false;
+        // Only store odd numbers
+        vector<bool> prime(n, true);
 
-        int count = 0;
+        prime[0] = prime[1] = false;
 
-        for (int i = 2; i < n; i++) {
+        // 2 is prime
+        int count = 1;
 
-            if (isPrime[i]) {
-                count++;
+        for (int i = 3; (long long)i * i < n; i += 2) {
 
-                if ((long long)i * i < n) {
-                    for (int j = i * i; j < n; j += i) {
-                        isPrime[j] = false;
-                    }
+            if (prime[i]) {
+
+                // Only mark odd multiples
+                for (long long j = (long long)i * i;
+                     j < n;
+                     j += 2 * i) {
+
+                    prime[j] = false;
                 }
             }
+        }
+
+        // Count odd primes
+        for (int i = 3; i < n; i += 2) {
+            if (prime[i])
+                count++;
         }
 
         return count;
