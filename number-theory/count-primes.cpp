026f43ad8b6ed479ba34c3791e72b51416264056
yuktha809 <yuktha809@gmail.com>
2026-09-06 15:01:04 +0530

    LC-count-primes [Runtime: N/A; Memory: N/A]

--- number-theory/count-primes.cpp
+++ number-theory/count-primes.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        vector<bool> nums(n,true);
         if(n<=2) return 0;
-        nums[0]=false;
-        nums[1]=false;
+        vector<bool> nums(n,true);
+        
+        nums[0]=nums[1]false;
+      
         for(int i=2;i*i<n;i++){
             if(nums[i]){
                 for(int j=i*i;j<n;j+=i){
