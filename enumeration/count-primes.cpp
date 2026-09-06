01a286c148fc238a8dc97f2ceaae082b3e5d6ce9
yuktha809 <yuktha809@gmail.com>
2026-09-06 15:05:01 +0530

    LC-count-primes [Runtime: N/A; Memory: N/A]

--- enumeration/count-primes.cpp
+++ enumeration/count-primes.cpp
@@ -5,23 +5,22 @@ public:
         if (n <= 2)
             return 0;
 
-        vector<bool> prime(n, true);
-
-        prime[0] = prime[1] = false;
-
-        for (long long i = 2; i * i < n; i++) {
-            if (prime[i]) {
-                for (long long j = i * i; j < n; j += i) {
-                    prime[j] = false;
-                }
-            }
-        }
+        vector<bool> isPrime(n, true);
+        isPrime[0] = isPrime[1] = false;
 
         int count = 0;
 
         for (int i = 2; i < n; i++) {
-            if (prime[i])
+
+            if (isPrime[i]) {
                 count++;
+
+                if ((long long)i * i < n) {
+                    for (int j = i * i; j < n; j += i) {
+                        isPrime[j] = false;
+                    }
+                }
+            }
         }
 
         return count;
