9c027960992d81f45a5480df92abb36e2ace4a95
yuktha809 <yuktha809@gmail.com>
2026-09-06 15:02:29 +0530

    LC-count-primes [Runtime: N/A; Memory: N/A]

--- primality-test/count-primes.cpp
+++ primality-test/count-primes.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     int countPrimes(int n) {
-        if(n<=2) return 0;
-        vector<bool> nums(n,true);
-        
-        nums[0]=nums[1]=false;
-      
-        for(long long i=2;i*i<n;i++){
-            if(nums[i]){
-                for(long long j=i*i;j<n;j+=i){
-                    nums[j]=false;
+
+        if (n <= 2)
+            return 0;
+
+        vector<bool> prime(n, true);
+
+        prime[0] = prime[1] = false;
+
+        for (int i = 2; i * i < n; i++) {
+
+            if (prime[i]) {
+
+                for (int j = i * i; j < n; j += i) {
+                    prime[j] = false;
                 }
             }
         }
-        int count =0;
-        for(int i = 2;i<n;i++){
-            if(nums[i]){
-                count++;
-            }
-        }
-        return count;
+
+        return count(prime.begin(), prime.end(), true);
     }
 };
